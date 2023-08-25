80782113dfe6170198cd6640b794f2395d0817f6
NicolasHasbun <Ni.Hasbun.Molina@gmail.com>
2023-08-25 04:12:53 -0400

    arreglos en carrito

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import Title from '../Title';
+import ItemCount from '../ItemCount';
 
 export const ItemListContainer = ({ texto }) => {
+
+    const onAdd = (quantity) => {
+        console.log(`compraste ${quantity} unidades`);
+    }
     return (
+        <>
         <Title greeting={texto}/>
+        <ItemCount initial={1} stock={5} onAdd={onAdd} />
+        </>
     );
 }
 
