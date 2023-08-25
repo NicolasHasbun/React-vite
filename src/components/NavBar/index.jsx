f49103794e2870a3fc6a2760d44f4fb02117b75b
NicolasHasbun <Ni.Hasbun.Molina@gmail.com>
2023-08-25 03:23:24 -0400

    arreglo de errores

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import CartWidget from '../CartWidget';
+
+export const NavBar = () => {
+    return (
+        <div className="container">
+            <nav className="nav">
+                <div className="nav__brand">
+                    <a className="nav__link" href="#">Mi Marca</a>
+                </div>
+                <ul className="nav__list">
+                    <li>
+                        <a className="nav__link" href="#">Categoria 1</a>
+                    </li>
+                    <li>
+                        <a className="nav__link" href="#">Categoria 2</a>
+                    </li>
+                    <li>
+                        <a className="nav__link" href="#">
+                            <CartWidget/>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default NavBar;
