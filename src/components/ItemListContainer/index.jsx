5d7f5403ea11664eae2021356bb8244de2298ce9
NicolasHasbun <Ni.Hasbun.Molina@gmail.com>
2023-08-27 03:15:07 -0400

    se actualiza contador de carrito

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -4,11 +4,11 @@ import ItemList from '../ItemList';
 import { useParams } from 'react-router-dom';
 
 const films = [
-    { id: 1, image: "https://logos-world.net/wp-content/uploads/2020/06/Assassins-Creed-Logo.png", category:'films' ,title: "Assasin creed" },
-    { id: 2, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Simvol-iz-dishonored.svg/2036px-Simvol-iz-dishonored.svg.png", category:'films' ,title: "Dishonored" },
-    { id: 3, image: "https://upload.wikimedia.org/wikipedia/commons/4/41/Blasphemous_logo.png", category:'films' ,title: "Blasphemus" },
-    { id: 4, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Animal_Crossing_Leaf.svg/504px-Animal_Crossing_Leaf.svg.png", category:'series' ,title: "Animal Crossing" },
-    { id: 5, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Zelda_2017.svg/800px-Zelda_2017.svg.png", category:'series' ,title: "Zelda" },
+    { id: 1, price:100, image: "https://logos-world.net/wp-content/uploads/2020/06/Assassins-Creed-Logo.png", category:'films' ,title: "Assasin creed" },
+    { id: 2, price:120, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Simvol-iz-dishonored.svg/2036px-Simvol-iz-dishonored.svg.png", category:'films' ,title: "Dishonored" },
+    { id: 3, price:130, image: "https://upload.wikimedia.org/wikipedia/commons/4/41/Blasphemous_logo.png", category:'films' ,title: "Blasphemus" },
+    { id: 4, price:140, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Animal_Crossing_Leaf.svg/504px-Animal_Crossing_Leaf.svg.png", category:'series' ,title: "Animal Crossing" },
+    { id: 5, price:150, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Zelda_2017.svg/800px-Zelda_2017.svg.png", category:'series' ,title: "Zelda" },
 
 ];
 
