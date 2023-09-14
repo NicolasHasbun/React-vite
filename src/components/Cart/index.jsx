4cdb61365ffe4c22c024a6b85de2c7efd57aed24
NicolasHasbun <Ni.Hasbun.Molina@gmail.com>
2023-09-14 19:46:18 -0300

    orden de compra a FireBase

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../../context/CartContext';
 import ItemCart from '../ItemCart';
 
 const Cart = () => {
     const { cart, totalPrice } = useCartContext();
+
+    const order = {
+        buyer: {
+            name: 'Pablo',
+            email: 'Pablo@gmail.com',
+            phone: '123123',
+            addres: 'asdd'
+        },
+        items: cart.map(product => ({ id: product.id, title: product.title, price: product.price, quantity: product.quantity})),
+        total: totalPrice(),
+    }
+
+    const handleClick = () => {
+        const db = getFirestore();
+        const ordersCollection = collection(db, 'orders');
+        addDoc(ordersCollection, order)
+        .then (({ id }) => console.log(id))
+    }
 
     if (cart.length === 0) {
         return(
@@ -25,6 +44,7 @@ const Cart = () => {
         <p>
             total: {totalPrice()}
         </p>
+        <button onClick={handleClick}>Emitir compra</button>
         </>
     );
 }
